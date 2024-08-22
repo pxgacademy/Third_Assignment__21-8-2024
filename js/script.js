53e7b76ff2591c0b2b4ac83d3fff8f589da65680
Abdullah <graphicdesigner.abdullah@gmail.com>
2024-08-22 17:13:17 +0600

    Nav section completed

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ tailwind.config = {
     extend: {
       colors: {
         clifford: "#da373d",
+        primaryColor: "#ABEF5F",
+        secondaryColor: "#abef5f80",
+        primaryTextColor: "#212121",
+        secondaryTextColor: "#737373",
+        bgColorOne: "#F9F9F9",
+        bgColorTwo: "#F3F3F3",
       },
       fontFamily: {
         manrope: ["Manrope", "sans-serif"],
